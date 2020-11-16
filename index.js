bcf15440640a7f485e5a90179194ec0b2115082a
Knightplayzz <73634415+Knightplayzz@users.noreply.github.com>
2020-11-16 16:26:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const fs = require ("fs");
 const bot = new discord.Client();
 bot.login(process.env.token);
 
-const activeSongs = new Map();
 
 var prefix = botConfig.prefix;
 bot.commands = new discord.Collection()
@@ -20,7 +19,7 @@ bot.user.setActivity("Your problems", {
 
 });
 
-fs.readdir("./commands/" , (err, files) => {
+fs.readdir("./Commands/" , (err, files) => {
     if (err) console.log(err);
     var jsFiles = files.filter(f => f.split(".").pop() === "js");
     if (jsFiles.length <=0) {
@@ -29,7 +28,7 @@ fs.readdir("./commands/" , (err, files) => {
     }
         
     jsFiles.forEach((f,i) => {
-        var fileGet = require(`./commands/${f}`);
+        var fileGet = require(`./Commands/${f}`);
         console.log(`De file ${f} is geladen`);
         bot.commands.set(fileGet.help.name, fileGet)
     })
@@ -43,9 +42,6 @@ bot.on("message", async message => {
     var msg = message.content.toLocaleLowerCase();
 
 
-
-
-    var prefix = botConfig.prefix;
     var messageArray = message.content.split(" ");
 
     var swearWords = JSON.parse(fs.readFileSync("./data/swearWords.json"));
@@ -108,22 +104,6 @@ bot.on("message", async message => {
     var arguments = messageArray.slice(1);
 
     var commands = bot.commands.get(command.slice(prefix.length));
-
-    var options = {
-        active: activeSongs
-    }
    
     if (commands) commands.run(bot, message, arguments, options);
-});
-
-bot.on("message", async message =>{
-    
-    if(message.author.bot) return;
-
-    if(message.channel.type == "dm") return;
-
-    var prefix = botConfig.prefix;
-
-    var messageArray = message.content.split(" ");
-
 });
